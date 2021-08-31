d20984ff7ed1ebdb438577d27aa30cff017af703
asheppard15 <83742214+asheppard15@users.noreply.github.com>
2021-08-31 15:15:25 -0400

    Added LinkedIn link

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
       </main>
       <footer className="App-footer">
         <small>
-        Coded by Andrea Sheppard and is open-sourced on{" "}<a href="https://github.com/asheppard14/dictionary-app" target="blank" rel="noopener noreferrer">GitHub</a>
+        Coded by{" "}<a href="https://www.linkedin.com/in/andrea-sheppard-53802b8a/">Andrea Sheppard</a> and is open-sourced on{" "}<a href="https://github.com/asheppard14/dictionary-app" target="blank" rel="noopener noreferrer">GitHub</a>
         </small>
       </footer>
       </div>
